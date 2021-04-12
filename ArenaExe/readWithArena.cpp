444bf3f02084f1a2f57fb14961a5068232680065
Hariharan Ramanathan <hariharan.r1999@gmail.com>
2021-04-12 18:15:43 +0530

    smartPtr

--- ArenaExe/readWithArena.cpp
+++ ArenaExe/readWithArena.cpp
@@ -8,20 +8,40 @@ using namespace google::protobuf;
 using namespace ArenaExe;
 using namespace std::chrono;
 
+template <class T>
+class smartPtr{
+    T* ptr;
+    public:
+        explicit smartPtr(T* p = NULL){
+            ptr = p;
+        }
+        ~smartPtr(){
+            cout<<"Destroyed"<<endl;
+            delete(ptr);
+        }
+        T* operator->(){
+            return ptr;
+        }
+
+        T& operator* (){
+            return *ptr;
+        }
+};
+
 Arena* createArena(int size, int total){
     Arena* arena = (Arena*)malloc(size * total);
     return arena;
 }
 
-void listStudents(allStudents& allStudentsDetails, Arena* studentArena){
+void listStudents(allStudents& allStudentsDetails, smartPtr<Arena>* studentArena){
 
     auto start = high_resolution_clock::now();
 
     int studentSize = allStudentsDetails.all_student_size();
-
     for(int i=0;i<studentSize;i++){
-
-        const student* s1 = new(&studentArena[i]) student(allStudentsDetails.all_student(i));
+        cout<<&studentArena[0]<<endl;
+        student* s1 = new(&studentArena[i]) student(allStudentsDetails.all_student(i));
+        cout<<"student Address "<<s1<<endl;
         Arena* guideByArena = createArena(sizeof(student::guideBy), s1->guide_by_size());
 
         cout<<"Name        : "<<s1->name()<<endl;
@@ -29,7 +49,9 @@ void listStudents(allStudents& allStudentsDetails, Arena* studentArena){
         cout<<"Class       : "<<s1->class_()<< " "<<s1->sec()<<endl;
         int count = s1->guide_by_size();
         for(int j=0;j<count-1;j++){
+
             const student::guideBy* g1 = new(&guideByArena[j]) student::guideBy(s1->guide_by(j));
+            cout<<"Guide Address "<<g1<<endl;
             if(g1->gtype() == student::PARENT){
                 cout<<"| \n ----->Guided By Parent"<<endl;
                 if(g1->ptype() == student::FATHER){
@@ -50,9 +72,9 @@ void listStudents(allStudents& allStudentsDetails, Arena* studentArena){
             
         }
     cout<<"======================================================"<<endl;
-
+    // free(&(*s1));
     }
-
+    
     auto end = high_resolution_clock::now();
 
     auto duration = duration_cast<microseconds>(end - start);
@@ -66,10 +88,10 @@ int main(int argc, char* argv[]){
     allstudentsdetails.ParseFromIstream(&ifs);
 
     int studentSize = allstudentsdetails.all_student_size();
+    smartPtr<Arena>studentArena((Arena*)createArena(sizeof(student), studentSize));
+    // Arena* studentArena = createArena(sizeof(student), studentSize);
 
-    Arena* studentArena = createArena(sizeof(student), studentSize);
-
-    listStudents(allstudentsdetails, studentArena);
+    listStudents(allstudentsdetails, &studentArena);
 
 
 
