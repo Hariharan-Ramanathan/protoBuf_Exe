02f7f571dc3f64dea3878c90488bb5e7779de84e
Hariharan Ramanathan <hariharan.r1999@gmail.com>
2021-04-12 19:18:05 +0530

    smartPtr2

--- ArenaExe/readWithArena.cpp
+++ ArenaExe/readWithArena.cpp
@@ -33,15 +33,10 @@ Arena* createArena(int size, int total){
     return arena;
 }
 
-void listStudents(allStudents& allStudentsDetails, smartPtr<Arena>* studentArena){
+void listStudents(smartPtr<student> s1){
 
-    auto start = high_resolution_clock::now();
 
-    int studentSize = allStudentsDetails.all_student_size();
-    for(int i=0;i<studentSize;i++){
-        cout<<&studentArena[0]<<endl;
-        student* s1 = new(&studentArena[i]) student(allStudentsDetails.all_student(i));
-        cout<<"student Address "<<s1<<endl;
+
         Arena* guideByArena = createArena(sizeof(student::guideBy), s1->guide_by_size());
 
         cout<<"Name        : "<<s1->name()<<endl;
@@ -49,7 +44,6 @@ void listStudents(allStudents& allStudentsDetails, smartPtr<Arena>* studentArena
         cout<<"Class       : "<<s1->class_()<< " "<<s1->sec()<<endl;
         int count = s1->guide_by_size();
         for(int j=0;j<count-1;j++){
-
             const student::guideBy* g1 = new(&guideByArena[j]) student::guideBy(s1->guide_by(j));
             cout<<"Guide Address "<<g1<<endl;
             if(g1->gtype() == student::PARENT){
@@ -69,17 +63,57 @@ void listStudents(allStudents& allStudentsDetails, smartPtr<Arena>* studentArena
             }
 
                 cout<<"----->Phone Number :"<<g1->number()<<endl;
-            
+            delete g1;
         }
-    cout<<"======================================================"<<endl;
-    // free(&(*s1));
-    }
     
-    auto end = high_resolution_clock::now();
 
-    auto duration = duration_cast<microseconds>(end - start);
-    cout<<"Duration with arena= "<<duration.count()<<"microSeconds"<<endl;
+   
 }
+
+                                                                                                                                                    // void listStudents2(allStudents& allStudentsDetails){
+
+                                                                                                                                                    //     auto start = high_resolution_clock::now();
+
+                                                                                                                                                    //     int studentSize = allStudentsDetails.all_student_size();
+                                                                                                                                                    //     for(int i=0;i<studentSize;i++){
+                                                                                                                                                    //         const student& s1 = allStudentsDetails.all_student(i);
+                                                                                                                                                    //         Arena* guideByArena = createArena(sizeof(student::guideBy), s1.guide_by_size());
+
+                                                                                                                                                    //         cout<<"Name        : "<<s1.name()<<endl;
+                                                                                                                                                    //         cout<<"Roll Number : "<<s1.rollnum()<<endl;
+                                                                                                                                                    //         cout<<"Class       : "<<s1.class_()<< " "<<s1.sec()<<endl;
+                                                                                                                                                    //         int count = s1->guide_by_size();
+                                                                                                                                                    //         for(int j=0;j<count-1;j++){
+                                                                                                                                                    //             const student::guideBy& g1 = s1.guide_by(j);
+                                                                                                                                                    //             if(g1.gtype() == student::PARENT){
+                                                                                                                                                    //                 cout<<"| \n ----->Guided By Parent"<<endl;
+                                                                                                                                                    //                 if(g1.ptype() == student::FATHER){
+                                                                                                                                                    //                     cout<<"------>Father Name :";
+                                                                                                                                                    //                 }
+                                                                                                                                                    //                 else{
+                                                                                                                                                    //                     cout<<"------>Mother Name :";
+
+                                                                                                                                                    //                 }
+                                                                                                                                                    //                 cout<<g1.name()<<endl;
+                                                                                                                                                    //             }
+                                                                                                                                                    //             else if(g1.gtype() == student::GUARDIAN){
+                                                                                                                                                    //                 cout<<"| \n ----->Guided By Guardian"<<endl;
+                                                                                                                                                    //                 cout<<"----->Guardian Name :"<<g1.name()<<endl;
+                                                                                                                                                    //             }
+
+                                                                                                                                                    //                 cout<<"----->Phone Number :"<<g1.number()<<endl;
+                                                                                                                                                    //             delete &g1;
+                                                                                                                                                    //         }
+                                                                                                                                                    //     cout<<"======================================================"<<endl;
+                                                                                                                                                    //     // free(&(*s1));
+                                                                                                                                                    //     // delete s1;
+                                                                                                                                                    //     }
+                                                                                                                                                        
+                                                                                                                                                    //     auto end = high_resolution_clock::now();
+
+                                                                                                                                                    //     auto duration = duration_cast<microseconds>(end - start);
+                                                                                                                                                    //     cout<<"Duration with arena= "<<duration.count()<<"microSeconds"<<endl;
+                                                                                                                                                    // }
 
 int main(int argc, char* argv[]){
 
@@ -88,12 +122,24 @@ int main(int argc, char* argv[]){
     allstudentsdetails.ParseFromIstream(&ifs);
 
     int studentSize = allstudentsdetails.all_student_size();
-    smartPtr<Arena>studentArena((Arena*)createArena(sizeof(student), studentSize));
-    // Arena* studentArena = createArena(sizeof(student), studentSize);
 
-    listStudents(allstudentsdetails, &studentArena);
+    Arena* studentArena = createArena(sizeof(student), studentSize);
 
+    auto start = high_resolution_clock::now();
+    
+    for(int i=0;i<studentSize;i++){
+        smartPtr<student>s1(new(&studentArena[i]) student(allstudentsdetails.all_student(i)));
+        listStudents(s1);
+        cout<<"======================================================"<<endl;
+    }
+     auto end = high_resolution_clock::now();
 
+    auto duration = duration_cast<microseconds>(end - start);
+    cout<<"Duration with arena= "<<duration.count()<<"microSeconds"<<endl;
+    
+    // listStudents2(allstudentsdetails);
+
+    delete studentArena;
 
     return 0;
 }
